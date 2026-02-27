6b125b24f6f23c9a03046f5c0c1a391b05a57249
AgentR <bio.ai.dev@gmail.com>
2026-02-27 13:29:53 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-const CACHE_NAME = "fff-cache-v4";
+const CACHE_NAME = "fff-cache-v2";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./splash.png",
-  "./logo2.png",
-  "./taktikboard.png",
-  "./header_print.png",
+  "./taktikboard.jpg"
   "./blank_form.pdf"
 ];
 
@@ -16,22 +14,6 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
